722932de066be32c538e32c6cd8c433c11522525
diazarm <marceloardiaz@gmail.com>
2024-01-21 13:50:18 -0300

    learning serverts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-require('dotenv').config();
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+require('dotenv').config();
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(morgan('dev'))
-app.use(cors())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.listen(PORT)
 console.log(`Server in listening on port ${PORT} `);

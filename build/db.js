5d5c409c663dc0f46782351e2f3b39bb292fd0d0
diazarm <marceloardiaz@gmail.com>
2024-01-22 07:54:10 -0300

    working

--- build/db.js
+++ build/db.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: "DB_HOST",
-    port: "DB_PORT",
-    username: "postgres",
-    password: "test",
-    database: "test",
+    host: DB_HOST,
+    port: 5432,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     synchronize: true,
     logging: true,
     entities: [],
--- build/index.js
+++ build/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,8 +15,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 require("dotenv").config();
 const app_1 = __importDefault(require("./app"));
+const db_1 = require("./db");
+require("dotenv").config();
+const { DB_NAME } = process.env;
 const port = 3000;
-app_1.default.listen(port, () => {
-    console.log(`Server in listening on port ${port}`);
-});
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield db_1.AppDataSource.initialize();
+            console.log(`db ${DB_NAME} connection OK`);
+            app_1.default.listen(port, () => {
+                console.log(`Server in listening on port ${port}`);
+            });
+        }
+        catch (error) {
+            console.error("Initialization error", error);
+        }
+    });
+}
+main();
 //nota : la compilacion se hace manual con tsc y eso nos permite luego usar node build/index.js
